a54842976869c06c331332744e1d96d5b897999c
Animesh101-wondercar <animeshjha101@gmail.com>
2023-10-09 00:13:33 +0530

    more add ons

--- test.cpp
+++ test.cpp
@@ -8,5 +8,11 @@ int main(){
     for(auto it:vt){
         cout<<" "<<it;
     }
+    for(int i=0;i>2;){
+        cout<<"";
+        if(1==0)
+        continue;
+        else break;
+    }
     return 0;
 }
